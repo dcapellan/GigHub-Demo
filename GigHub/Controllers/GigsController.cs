0ef07c6dab8d2866aabdcce6c539fb29a56f083c
Denny Capellan <denny_capellan@hotmail.com>
2017-12-30 09:25:08 -0500

    Add Gigs

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -8,7 +10,7 @@ namespace GigHub.Controllers
     public class GigsController : Controller
     {
 
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public GigsController()
         {
@@ -17,6 +19,7 @@ namespace GigHub.Controllers
         
 
         // GET: Gigs
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -26,5 +29,29 @@ namespace GigHub.Controllers
 
             return View(viewModel);
         }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var d = viewModel.Date;
+
+            var gig = new Gig
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+        
     }
 }
